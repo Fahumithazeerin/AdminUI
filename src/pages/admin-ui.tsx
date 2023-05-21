7d1ef9547a698f715759d145d1a92b3aef5828a0
Fahumitha <125856740+Fahumitha@users.noreply.github.com>
2023-05-21 19:08:51 +0530

    feat: User info edit and save

--- src/pages/admin-ui.tsx
+++ src/pages/admin-ui.tsx
@@ -5,6 +5,7 @@ import { deleteSelectedUser, deleteSelectedUserMainCheckBox, fetchData } from ".
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
+
 interface Data {
    id: number;
    name: string;
@@ -26,7 +27,8 @@ interface AdminDispatchProps {
 interface IOwnStateProps {
     checkBox: boolean,
     deleteCount: number,
-    editStatus: boolean
+    editStatus: boolean,
+    currentUser: number
 }
 
 const mapDispatchToProps = (dispatch: any) => ({
@@ -48,29 +50,38 @@ class Admin extends React.Component<Iprops, IOwnStateProps> {
         this.state = {
             checkBox: false,
             deleteCount: 0,
-            editStatus: false
+            editStatus: false,
+            currentUser: 0
         }
     }
   async componentDidMount(){
         await this.props.init();
          console.log(this.props.apiData);
     }
-   
-
 handleSelectAndDeSelectAll() { 
     const mainCheckbox = document.getElementById('main-checkbox') as HTMLInputElement;
     const subCheckbox = document.getElementsByClassName('sub-checkbox');
+    const row = document.getElementsByClassName('row');
+    const input = document.getElementsByClassName('input');
     const mainCheckboxState = mainCheckbox.checked;
     if(mainCheckboxState === true) {
         for(let i = 0; i < 10; i++) {
           const subCheckboxState = subCheckbox[i] as HTMLInputElement;
           subCheckboxState.checked = mainCheckboxState;
+          const rows= row[i] as HTMLInputElement
+          const inputs= input[i] as HTMLInputElement
+          rows.style.backgroundColor = 'lightgrey'
+          inputs.style.backgroundColor = 'lightgrey'
         }
     } else {
         const mainCheckboxState = false;
         for(let i = 0; i < 10; i++) {
             const subCheckboxState = subCheckbox[i] as HTMLInputElement;
             subCheckboxState.checked = mainCheckboxState;
+            const rows= row[i] as HTMLInputElement
+            const inputs= input[i] as HTMLInputElement
+            rows.style.backgroundColor = 'transparent'
+            inputs.style.backgroundColor = 'transparent'
         }
     }
 }
@@ -89,16 +100,28 @@ handleDeleteForMainCheckbox() {
 }
 
 handleContent(userId: number) {
+    const input = document.getElementsByClassName('input');
+    const length = userId * 4;
     if(!this.state.editStatus) {
-       const input = document.getElementsByClassName('input');
-       const length = userId * 4;
-        for(let i = length-4 ; i < length; i++) {
+       this.setState({editStatus: true, currentUser: userId})
+       for(let i = length-4 ; i < length; i++) {
          const readonly = input[i] as HTMLInputElement;
          readonly.readOnly = false;
-    }
+    } 
+  } else{
+    for(let i = length-4 ; i < length; i++) {
+        const readonly = input[i] as HTMLInputElement;
+        readonly.readOnly = true;
+   } 
   }
 }
-
+cal(userId: number) {
+    if(this.state.currentUser === userId) {
+        return true;
+    } else {
+        return false;
+    }
+}
     render() {
         return (
         
@@ -119,21 +142,22 @@ handleContent(userId: number) {
                     </thead>
                     <tbody>
                         {this.props.apiData? this.props.apiData.map((item: Data) => (
-                            <tr key={item.id}>
+                            <tr key={item.id} className="row">
                                 <td><input type='checkbox' className='sub-checkbox'></input></td>
                                 <td><input type='text' className='input' defaultValue={item.id}  readOnly></input></td>
                                 <td><input type='text' className='input' defaultValue={item.name}   readOnly></input></td>
                                 <td><input type='text' className='input' defaultValue={item.email}  readOnly></input></td>
                                 <td><input type='text' className='input' defaultValue={item.role}   readOnly></input></td>
                                 <td>
-                                    <FontAwesomeIcon 
+                                    { !this.state.editStatus || !this.cal(item.id) ? <FontAwesomeIcon 
                                         icon={faPenToSquare} 
                                         className='editIcons' 
                                         onClick={()=>{
                                             this.handleContent(item.id)
                                            }
                                         }
-                                    />
+                                    /> : <button onClick={()=>{this.setState({editStatus: false}); this.handleContent(item.id)}}>Save</button>
+                                    }
                                     <FontAwesomeIcon 
                                         icon={faTrashCan}
                                         className='icons' 
@@ -153,4 +177,5 @@ handleContent(userId: number) {
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(Admin);
+
 
